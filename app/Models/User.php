824049d0c6d4f6669cfbd7aa1434d699ceeed89f
fufuYang <imfuting@gmail.com>
2024-11-29 00:07:39 +0800

    feat(User): 更新 User 模型，新增討論串關聯和調整隱藏屬性

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,26 +2,21 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+/**
+ * @property int $subscription_type
+ */
 
 class User extends Authenticatable
 {
     use HasFactory;
     use Notifiable;
+    use HasApiTokens;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
 
     /**
      * The attributes that should be hidden for serialization.
@@ -44,5 +39,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+    // 一對多關聯：一個用戶可以有多個討論串（threads）
+    public function threads()
+    {
+        return $this->hasMany(Thread::class);
     }
 }
